46ecb3340c3a7f7c0589002bd029a8b09f0a7682
maeve-hyeeunna <nahef2021@gmail.com>
2026-03-07 14:48:23 -0500

    Update data.ts

--- data/home/aboutMe/data.ts
+++ data/home/aboutMe/data.ts
@@ -7,7 +7,7 @@ export type { IAboutMeItem } from "../type";
 export const ABOUT_ME_DATA: IAboutMeItem[] = [
   {
     id: "performance",
-    videoUrl: "https://vimeo.com/manage/videos/1171363290",
+    videoUrl: "https://vimeo.com/manage/videos/1171372270",
     title: "Performance & Stage Sound",
     description: [
       "I have engineered live performances across major concert halls and large rehearsal spaces at Berklee College of Music, including David Friend Recital Hall, Café 939, and the Berklee Performance Center. Through these experiences, I developed strong proficiency in digital mixing consoles, Dante-based audio networks, and live production workflows.",
